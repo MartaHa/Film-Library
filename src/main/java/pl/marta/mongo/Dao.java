4b00c865d33cf141c519a2bbeaf67a900f5d7d61
MartaHa <helbinmarta@gmail.com>
2018-11-12 14:40:23 +0100

    Exceptions added + some corrections

--- src/main/java/pl/marta/mongo/Dao.java
+++ src/main/java/pl/marta/mongo/Dao.java
@@ -2,33 +2,38 @@ package pl.marta.mongo;
 
 import com.mongodb.*;
 
+import java.net.UnknownHostException;
 import java.time.LocalDate;
 import java.util.*;
 
 public class Dao {
 
-    
+
     /* Add film */
 
-    public String addFilm(String title, String yearOfRelease, String director, String category, LocalDate created) {
+    public String addFilm(String title, String yearOfRelease, String director, String category, LocalDate created) throws UnknownHostException {
+        try {
 
-        /* Creating a connection with Mongo */
-        MongoClient mongoClient = new MongoClient("localhost", 27017);
-
-        /* fetching/creating a table */
-
-        DB database = mongoClient.getDB("films");
-        DBCollection table = database.getCollection("film");
+            /* Creating a connection with Mongo */
+            MongoClient mongoClient = new MongoClient("localhost", 27017);
 
 
-        BasicDBObject document = new BasicDBObject();
-        document.put("title", title);
-        document.put("yearOfRelease", yearOfRelease);
-        document.put("director", director);
-        document.put("category", category);
-        document.put("created", created);
-        table.insert(document);
+            /* fetching/creating a table */
 
+            DB database = mongoClient.getDB("films");
+            DBCollection table = database.getCollection("film");
+
+            BasicDBObject document = new BasicDBObject();
+            document.put("title", title);
+            document.put("yearOfRelease", yearOfRelease);
+            document.put("director", director);
+            document.put("category", category);
+            document.put("created", created);
+            table.insert(document);
+
+        } catch (MongoException e) {
+            e.printStackTrace();
+        }
         String reply = "Your film has been added";
         return reply;
     }
@@ -36,19 +41,24 @@ public class Dao {
 
     //viewing all films
 
-    public List showAll() {
+    public List showAll() throws UnknownHostException {
+        List<DBObject> allFilms = new ArrayList<>();
+        try {
 
-        /* Creating a connection with Mongo */
-        MongoClient mongoClient = new MongoClient("localhost", 27017);
+            /* Creating a connection with Mongo */
+            MongoClient mongoClient = new MongoClient("localhost", 27017);
 
-        /* fetching/creating a table */
+            /* fetching/creating a table */
 
-        DB database = mongoClient.getDB("films");
-        DBCollection table = database.getCollection("film");
+            DB database = mongoClient.getDB("films");
+            DBCollection table = database.getCollection("film");
 
-        /*getting all documents from the collection*/
+            /*getting all documents from the collection*/
 
-        List<DBObject> allFilms = table.find().toArray();
+            allFilms = table.find().toArray();
+        } catch (MongoException e) {
+            e.printStackTrace();
+        }
         return allFilms;
     }
 
@@ -56,73 +66,86 @@ public class Dao {
     /* viewing a film */
 
 
-    public List showFilm(String title) {
-
-        /* Creating a connection with Mongo */
-        MongoClient mongoClient = new MongoClient("localhost", 27017);
-
-        /* fetching/creating a table */
-
-        DB database = mongoClient.getDB("films");
-        DBCollection table = database.getCollection("film");
-
-        //displaying the data
-
-        BasicDBObject searchQuery = new BasicDBObject();
-        searchQuery.put("title", title);
-
-        DBCursor cursor = table.find(searchQuery);
-
+    public List showFilm(String title) throws UnknownHostException{
         List filmAtributes = new ArrayList();
-        while (cursor.hasNext()) {
-            filmAtributes.add(cursor.next());
+        try {
+            /* Creating a connection with Mongo */
+            MongoClient mongoClient = new MongoClient("localhost", 27017);
+
+            /* fetching/creating a table */
+
+            DB database = mongoClient.getDB("films");
+            DBCollection table = database.getCollection("film");
+
+            //displaying the data
+
+            BasicDBObject searchQuery = new BasicDBObject();
+            searchQuery.put("title", title);
+
+            DBCursor cursor = table.find(searchQuery);
+
+
+            while (cursor.hasNext()) {
+                filmAtributes.add(cursor.next());
+            }
+        } catch (MongoException e) {
+            e.printStackTrace();
         }
         return filmAtributes;
     }
 
 
-    public String updateFilm(String title, String toChange, String changed) {
+    public String updateFilm(String title, String toChange, String changed) throws UnknownHostException {
 
-        /* Creating a connection with Mongo */
-        MongoClient mongoClient = new MongoClient("localhost", 27017);
+        try {
+            /* Creating a connection with Mongo */
+            MongoClient mongoClient = new MongoClient("localhost", 27017);
 
-        /* fetching/creating a table */
+            /* fetching/creating a table */
 
-        DB database = mongoClient.getDB("films");
-        DBCollection table = database.getCollection("film");
-
-
-        // creating a document with updated data
-
-        BasicDBObject updatedDocument = new BasicDBObject();
-        updatedDocument.put(toChange, changed);
+            DB database = mongoClient.getDB("films");
+            DBCollection table = database.getCollection("film");
 
 
-        //search document where title=?
+            // creating a document with updated data
 
-        BasicDBObject searchQuery = new BasicDBObject();
-        searchQuery.put("title", title);
+            BasicDBObject updatedDocument = new BasicDBObject();
+            updatedDocument.put(toChange, changed);
 
-        table.update(searchQuery, updatedDocument);
+
+            //search document where title=?
+
+            BasicDBObject searchQuery = new BasicDBObject();
+            searchQuery.put("title", title);
+
+            table.update(searchQuery, updatedDocument);
+        } catch (MongoException e) {
+            e.printStackTrace();
+        }
         return "The film" + title + " has been updated succesfully";
     }
 
-    public String delete(String title) {
 
-        /* Creating a connection with Mongo */
-        MongoClient mongoClient = new MongoClient("localhost", 27017);
+    public String delete(String title) throws UnknownHostException {
 
-        /* fetching/creating a table */
+        try {
+            /* Creating a connection with Mongo */
+            MongoClient mongoClient = new MongoClient("localhost", 27017);
 
-        DB database = mongoClient.getDB("films");
-        DBCollection table = database.getCollection("film");
+            /* fetching/creating a table */
 
-        //search document where title=?
+            DB database = mongoClient.getDB("films");
+            DBCollection table = database.getCollection("film");
 
-        BasicDBObject searchQuery = new BasicDBObject();
-        searchQuery.put("title", title);
+            //search document where title=?
 
-        table.remove(searchQuery);
+            BasicDBObject searchQuery = new BasicDBObject();
+            searchQuery.put("title", title);
+
+            table.remove(searchQuery);
+        } catch (MongoException e) {
+            e.printStackTrace();
+        }
         return "Film: " + title + " has been deleted";
 
     }
--- src/main/java/pl/marta/mongo/Main.java
+++ src/main/java/pl/marta/mongo/Main.java
@@ -1,5 +1,6 @@
 package pl.marta.mongo;
 
+import java.net.UnknownHostException;
 import java.time.LocalDate;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.Scanner;
 public class Main {
 
 
-    public static void main(String[] args) throws InputMismatchException {
+    public static void main(String[] args) throws InputMismatchException, UnknownHostException {
 
 
         Scanner scanner = new Scanner(System.in);
@@ -26,38 +27,62 @@ public class Main {
                 "\n quit : Q");
 
         while (true) {
-            String answer = scanner.next();
+            String answer = "";
+            try {
+                answer = scanner.next();
+            } catch (InputMismatchException e) {
+                System.out.println("Not a proper format. Try again");
+            }
+            /* ADDING A FILM TO DATABASE */
+
             if (answer.equals("ADD")) {
 
+                String title = "";
+                String yearOfRelease = "";
+                String director = "";
+                String category = "";
+
+
                 /* getting data from user and adding the film to MongoDB */
+                try {
+                    System.out.println("Enter the film title:");
+                    title = scanner.next();
+                    System.out.println("Enter the year of release:");
+                    yearOfRelease = scanner.next();
+                    System.out.println("Enter the director:");
+                    director = scanner.next();
+                    System.out.println("Enter the category");
+                    category = scanner.next();
 
-                System.out.println("Enter the film title:");
-                String title = scanner.next();
-                System.out.println("Enter the year of release:");
-                String yearOfRelease = scanner.next();
-                System.out.println("Enter the director:");
-                String director = scanner.next();
-                System.out.println("Enter the category");
-                String category = scanner.next();
-                LocalDate created = LocalDate.now();
-
+                } catch (InputMismatchException e) {
+                    System.out.println("Wrong value, try again");
+                }
                 /* adding a  film to database */
 
+                LocalDate created = LocalDate.now();
                 System.out.println(dao.addFilm(title, yearOfRelease, director, category, created));
             }
 
-            /*show all list of films */
+            /*SHOW ALL LIST OF FILMS */
 
             else if (answer.equals("ALL")) {
                 List filmList = dao.showAll();
                 filmList.forEach(System.out::println);
 
+
+
+                /* SHOW ONE FILM  */
+
             } else if (answer.equals("SHOW")) {
 
                 // getting the name of the film to show
-
+                String title = "";
                 System.out.println("Please enetr the title of the film you want to see");
-                String title = scanner.next();
+                try {
+                    title = scanner.next();
+                } catch (InputMismatchException e) {
+                    System.out.println("That is not a proper value try again");
+                }
 
 
                 /* showing one film */
@@ -66,39 +91,66 @@ public class Main {
                 showFilm.forEach(System.out::println);
 
 
+                /* UPDATE METHOD */
+
+
             } else if (answer.equals("UPDATE")) {
 
                 // getting the name of the film to update
-
+                String title = "";
                 System.out.println("Please enetr the title of the film you want to update");
-                String title = scanner.next();
-
+                try {
+                    title = scanner.next();
+                } catch (InputMismatchException e) {
+                    System.out.println("That is not a proper format. Try again!");
+                }
 
                 /* updating */
 
                 System.out.println("Enter the data for updating");
 
                 while (scanner.next() != "q") {
+                    String toChange = "";
+                    String changed = "";
+
                     System.out.println("What do you want to change ? // for exiting type 'q':");
-                    String toChange = scanner.next();
+                    try {
+                        toChange = scanner.next();
+                    } catch (InputMismatchException e) {
+                        System.out.println("That is not a proper format. Try again!");
+                    }
                     System.out.println("What do you want to type instead:");
-                    String changed = scanner.next();
+                    try {
+                        changed = scanner.next();
+                    } catch (InputMismatchException e) {
+                        System.out.println("That is not a proper format. Try again!");
+
+                    }
                     dao.updateFilm(title, toChange, changed);
 
                 }
 
-                /* delete the film */
+
+
+                /* DELETE METHOD */
 
             } else if (answer.equals("DELETE")) {
 
+                String title = "";
                 System.out.println("Enter the title of the film to delete:");
-                String title = scanner.next();
-                dao.delete(title);
+                try {
+                    title = scanner.next();
+                    dao.delete(title);
+                } catch (InputMismatchException e) {
+                    System.out.println("That is not a proper value Try again");
+
+                }
             } else if (answer.equals("Q")) {
                 System.out.println("Goodbye!");
                 break;
             } else {
                 System.out.println("It's not a proper command try again");
+
             }
 
 
